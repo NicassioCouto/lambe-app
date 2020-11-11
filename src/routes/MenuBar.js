165f501973806aefbf2fb87c86f59ddd7bab22f8
Cancio <filipe.cancio@outlook.com>
2020-11-11 17:17:41 -0300

    feat: estilizando acesso a rotas de busca #2

--- src/routes/MenuBar.js
+++ src/routes/MenuBar.js
@@ -5,6 +5,7 @@ import User from '../pages/User';
 import Client from '../pages/Client';
 import Product from '../pages/Product';
 import Job from '../pages/Job';
+import { MaterialIcons} from '@expo/vector-icons';
 
 const {Navigator,Screen} = createBottomTabNavigator();
 
@@ -12,11 +13,74 @@ function MenuBar(){
     return(
         <>
         <NavigationContainer>
-            <Navigator screenOptions={{headerShown:false}}>
-                <Screen name="User" component={User}/>
-                <Screen name="Client" component={Client}/>
-                <Screen name="Product" component={Product}/>
-                <Screen name="Job" component={Job}/>
+            <Navigator 
+                    tabBarOptions={{
+                        style:{
+                            elevation:0,
+                            borderTopLeftRadius:30,
+                            borderTopRightRadius:30,
+                            backgroundColor:'#3D3D3DCC',
+                            backdropFilter:'blur(10px)',
+                        },
+                        tabStyle:{
+                            alignItems:'center',
+                            justifyContent: 'center'
+                        },
+                        iconStyle:{
+                            flex:0,
+                            width:20,
+                            height:20,
+                        },
+                        showLabel:false,
+                        inactiveTintColor: '#E8E8E8',
+                        activeTintColor: '#FFB9DA',
+                    }}
+                >
+                <Screen 
+                    name="User" 
+                    component={User} 
+                    options={{
+                        tabBarIcon: ({size,color})=>{
+                            return(
+                                <MaterialIcons name="person" size={size} color={color}/>
+                            )
+                        }
+                    }}
+                />
+                <Screen 
+                    name="Client" 
+                    component={Client}
+                    options={{
+                        tabBarIcon: ({size,color})=>{
+                            return(
+                                <MaterialIcons name="style" size={size} color={color}/>
+                            )
+                        }
+                    }}
+                />
+                <Screen 
+                    name="Product" 
+                    component={Product}
+                    options={{
+                        tabBarIcon: ({size,color})=>{
+                            return(
+                                <MaterialIcons name="supervisor-account" size={size} color={color}/>
+                            )
+                        }
+                    }}
+                />
+                <Screen 
+                    name="Job" 
+                    component={Job}
+                    options={{
+                        
+                        tabBarIcon: ({size,color})=>{
+                            return(
+                                <MaterialIcons name="color-lens" size={size} color={color}/>
+                            )
+                        }
+                    }}
+                />
             </Navigator>
         </NavigationContainer>
         </>
