b6b80cb3c8c19bc62dbc945b38f47f208d82cdd2
Cancio <filipe.cancio@outlook.com>
2020-11-14 12:03:46 -0300

    Feat: Criando rota de entidade #2

--- src/components/Organisms/Entity/index.js
+++ src/components/Organisms/Entity/index.js
@@ -1,16 +1,43 @@
 import React from 'react'
 import { View,Text,StyleSheet } from 'react-native'
 import ButtonAction from "../../atoms/ButtonAction";
+import CircleAvatar from "../../atoms/CircleAvatar";
+import {TouchableOpacity} from "react-native-gesture-handler";
+import {useNavigation} from "@react-navigation/native";
+import ScrollContent from "../../molecules/ScrollContent";
 
 
 function index(props){
 
+    const {navigate} = useNavigation();
+
+    function handleUpdate(){
+        navigate('Update');
+    }
+    function handleDelete(){
+        navigate('Delete');
+    }
+
+
     return (
         <>
+            <ScrollContent>
             <View style={styles.container}>
-                <Text style={styles.title}>Entity {props.entity}</Text>
-                <ButtonAction />
+                <CircleAvatar source={props.avatar} size={180}/>
+                <Text style={styles.title}>{props.title}</Text>
+                <Text style={styles.subtitle}>{props.subtitle}</Text>
+                <TouchableOpacity onPress={handleUpdate}>
+                    <Text style={styles.link}>Atualizar Cadastro</Text>
+                </TouchableOpacity>
+                {props.children}
+                {props.delete && (
+                    <TouchableOpacity onPress={handleDelete}>
+                        <Text style={styles.link}>Deletar Cadastro</Text>
+                    </TouchableOpacity>
+                )}
             </View>
+            </ScrollContent>
+            <ButtonAction />
         </>
     )
 }
@@ -24,11 +51,26 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
     },
-    title:{
+    title: {
         color: '#FFFFFF',
         fontSize: 30,
         textAlign: 'center',
+        marginTop: 14,
+    },
+    subtitle:{
+        color: '#FFFFFF',
+        fontSize: 20,
+        textAlign: 'center',
+        marginTop: 14,
+    },
+    text:{
+        color: '#FFFFFF',
+        marginTop: 14,
+    },
+    link:{
+        color: '#FF5CAA',
+        textAlign: 'center',
     }
-});
+    });
 
 export default index;
