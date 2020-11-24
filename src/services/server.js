40842a8f58a38af0b37d3c60862a1d711abce272
Cancio <filipe.cancio@outlook.com>
2020-11-24 04:17:34 -0300

    update server layer

--- src/services/server.js
+++ src/services/server.js
@@ -2,17 +2,35 @@ import axios from 'axios';
 
 const base_url = 'https://orbona.appspot.com/';
 
-async function getData(node_id) {
+async function getData(entity) {
   try {
-    const res = await (await axios.get(`${base_url}/${node_id}`));
+    const res = await (await axios.get(`${base_url}/${entity}`));
     return res.data;
   } catch (err) {
     console.log(err.message);
   }
 }
-async function createData(node_id) {
+export async function createData(entity,node_id,body_data) {
   try {
-    const res = await (await axios.get(`${base_url}/create/${node_id}`));
+    const res = await (await axios.post(`${base_url}/${entity}/${node_id}`, body_data));
+    return res.data;
+  } catch (err) {
+    console.log(err.message);
+  }
+}
+
+export async function updateData(entity,node_id,body_data) {
+  try {
+    const res = await (await axios.put(`${base_url}/${entity}/${node_id}`, body_data));
+    return res.data;
+  } catch (err) {
+    console.log(err.message);
+  }
+}
+
+export async function deleteData(entity,node_id) {
+  try {
+    const res = await (await axios.delete(`${base_url}/${entity}/${node_id}`));
     return res.data;
   } catch (err) {
     console.log(err.message);
